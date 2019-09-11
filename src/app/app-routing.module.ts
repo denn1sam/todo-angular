02f3e9c1fbcd821981785a824a2aa03260f96e74
Denys Samotos <denys.samotos@dat.com>
2019-09-12 00:00:36 +0300

    CREATE new components and ADD styles

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,6 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: '',
-    loadChildren: './list/list.module#ListModule',
+    loadChildren: './todo/list/list.module#ListModule',
   },
 ];
--- src/app/todo/list/list.module.ts
+++ src/app/todo/list/list.module.ts
@@ -1,25 +1,37 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import {
   MatToolbarModule,
   MatIconModule,
   MatButtonModule,
   MatCardModule,
+  MatFormFieldModule,
+  MatInputModule,
 } from '@angular/material';
 import { RouterModule } from '@angular/router';
 
 import { listRoutes } from './list-routing.module';
 import { ListComponent } from './list.component';
+import { InputComponent } from '../input/input.component';
+import { ListItemComponent } from '../list-item/list-item.component';
 
 @NgModule({
   imports: [
     CommonModule,
     RouterModule.forChild(listRoutes),
+    FormsModule,
     MatToolbarModule,
     MatIconModule,
     MatButtonModule,
-    MatCardModule
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
   ],
-  declarations: [ListComponent]
+  declarations: [
+    ListComponent,
+    InputComponent,
+    ListItemComponent,
+  ]
 })
 export class ListModule { }
